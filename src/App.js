a64fcd44aaf0f0b100eab1b854f442a7202a91d1
Arifkhankhankhankhan-Git <arifkhan93246057@gmail.com>
2024-04-29 00:09:48 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+    <div className="container">
+        <div class="tri2">
+   
+        </div>
+        <div className="tri3"></div>
+    </div>
+        
+    </div>
+  );
+};
+
+export default App;
+
